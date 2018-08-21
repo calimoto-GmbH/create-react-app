11d6f6891cd38961bc2b4960608d1db3bf9b4e8e
Vincent Semrau <vincent.semrau@calimoto.eu>
2018-08-21 14:12:28 +0200

    Put dynamic imported module's style into the css chunk file instead of putting it into the dynamic module. Optional DONT_ANALIZE_BUNDLE.

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -55,6 +55,12 @@ const extractTextPluginOptions = shouldUseRelativeAssetPaths
     { publicPath: Array(cssFilename.split('/').length).join('../') }
   : {};
 
+const optionalPlugins = [];
+if(!(process.env.DONT_ANALIZE_BUNDLE == "true")){
+  optionalPlugins.push(new BundleAnalyzerPlugin());
+}
+
+
 // This is the production configuration.
 // It compiles slowly and is focused on producing a fast and minimal bundle.
 // The development configuration is different and lives in a separate file.
@@ -317,6 +323,7 @@ module.exports = {
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: cssFilename,
+      allChunks: true
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
@@ -360,7 +367,7 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new BundleAnalyzerPlugin()
+    ...optionalPlugins
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
